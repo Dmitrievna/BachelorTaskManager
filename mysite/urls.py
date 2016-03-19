8c84c910e32504a3a5dbf16f461ff26172821515
girlbananan <buduprogramistom@gmail.com>
2016-03-19 19:46:43 -0400

    templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
